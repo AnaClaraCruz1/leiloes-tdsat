37c17c98b5c0dc6d677e96632a54a13096e13482
Ana Clara <annacllara20@hotmail.com>
2026-05-24 17:00:16 -0300

    Implementação da funcionalidade de venda de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -78,7 +78,30 @@ public class ProdutosDAO {
         }
 
         return listagem;
-    }  
+    }
+    public void venderProduto(int id){
+
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+
+            prep = conn.prepareStatement(sql);
+
+            prep.setInt(1, id);
+
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+        } catch (Exception erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + erro);
+
+        }
+
+    }
         
 }
 
